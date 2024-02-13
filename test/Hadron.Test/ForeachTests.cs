2db28817cabe52f646bace8fa6b12daefae9d5c4
le-nn <renpei.okada@outlook.com>
2024-02-14 07:38:28 +0900

    Add test for range expression foreach

--- test/Hadron.Test/ForeachTests.cs
+++ test/Hadron.Test/ForeachTests.cs
@@ -33,4 +33,52 @@ public class ForeachTests {
         // Assert
         expected.Should().BeEquivalentTo(actual);
     }
+
+    [Fact]
+    public void Foreach_ShouldEnumerateAllElements3() {
+        // Act
+        var actual = new List<int>();
+        foreach (var i in 0..10) {
+            actual.Add(i);
+        }
+
+        // Assert
+        actual.Should().BeEquivalentTo((int[])[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+
+    [Fact]
+    public void Foreach_ShouldEnumerateAllElements4() {
+        // Act
+        var actual = new List<int>();
+        foreach (var i in 5..10) {
+            actual.Add(i);
+        }
+
+        // Assert
+        actual.Should().BeEquivalentTo((int[])[5, 6, 7, 8, 9, 10]);
+    }
+
+    [Fact]
+    public void Foreach_ShouldEnumerateAllElements5() {
+        // Act
+        var actual = new List<int>();
+        foreach (var i in 0..^10) {
+            actual.Add(i);
+        }
+
+        // Assert
+        actual.Should().BeEquivalentTo((int[])[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    [Fact]
+    public void Foreach_ShouldEnumerateAllElements6() {
+        // Act
+        var actual = new List<int>();
+        foreach (var i in 5..^10) {
+            actual.Add(i);
+        }
+
+        // Assert
+        actual.Should().BeEquivalentTo((int[])[5, 6, 7, 8, 9]);
+    }
 }
