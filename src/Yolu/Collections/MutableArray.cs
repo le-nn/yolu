51bfa4a3aba0ae453d68767949b19512e4968626
le-nn <le.dev.nn@outlook.com>
2024-07-22 17:34:21 +0900

    Add debug view

--- src/Yolu/Collections/MutableArray.cs
+++ src/Yolu/Collections/MutableArray.cs
@@ -21,11 +21,33 @@ public static class MutableArrayBuilder {
     public static MutableArray<T> CreateMutableArray<T>(ReadOnlySpan<T> span) => new(span);
 }
 
+internal sealed class ICollectionDebugView<T> {
+    private readonly ICollection<T> _collection;
+
+    public ICollectionDebugView(ICollection<T> collection) {
+        if (collection == null) {
+            throw new ArgumentNullException(nameof(collection));
+        }
+
+        _collection = collection;
+    }
+
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public T[] Items {
+        get {
+            T[] items = new T[_collection.Count];
+            _collection.CopyTo(items, 0);
+            return items;
+        }
+    }
+}
+
 /// <summary>
 /// Represents a mutable array collection and implements <see cref="IMutableArray{T}"/>.
 /// </summary>
 /// <typeparam name="T">The type of elements in the array.</typeparam>
 [CollectionBuilder(typeof(MutableArrayBuilder), nameof(MutableArrayBuilder.CreateMutableArray))]
+[DebuggerTypeProxy(typeof(ICollectionDebugView<>))]
 public class MutableArray<T> : List<T>, IMutableArray<T> {
     public static MutableArray<T> Empty { get; } = [];
 
